b2d6158be0d9550efbef5f682e842a3b83b52e68
juniormt10 <junior.emt10@gmail.com>
2023-10-25 17:17:24 -0500

    25/10/2023

--- php/instructor_guardar.php
+++ php/instructor_guardar.php
@@ -10,29 +10,57 @@ $apellido=limpiar_cadena($_POST['apellidos']);
 $instruccion=limpiar_cadena($_POST['grado_instruccion']);
 $celular=limpiar_cadena($_POST['celular']);
 $correo=limpiar_cadena($_POST['correo']);
-$firma=limpiar_cadena($_FILES['firma_digital']['name']);
+$firma=limpiar_cadena($_FILES['firma_digital']['name']);//nombre del archivo
 
 
-if($nombre==""){
-    echo "!OOPSSS OCURRIO UN ERROR!,El nombre es obligatorio completar,error";
-    exit();
+
+
+// if($nombre==""){
+//     echo "!OOPSSS OCURRIO UN ERROR!,El nombre es obligatorio completar,error";
+//     exit();
+// }
+// if($apellido==""){
+//     echo "!OOPSSS OCURRIO UN ERROR!,El apellido es obligatorio completar,error";
+//     exit();
+// }
+// if($celular==""){
+//     echo "!OOPSSS OCURRIO UN ERROR!,El celular es obligatorio completar,error";
+//     exit();
+// }
+// if($instruccion==""){
+//     echo "!OOPSSS OCURRIO UN ERROR!,El instrucctor es obligatorio completar,error";
+//     exit();
+// }
+// if(empty($firma)){
+//     echo "!OOPSSS OCURRIO UN ERROR!,El firmar es obligatorio completar,error";
+//     exit();
+// }
+
+// validador para almacenar un archivo o imagen 
+
+$url_temporal = $_FILES["firma_digital"]["tmp_name"]; //Ruta temporal a donde se carga el archivo 
+$ruta_insertada = dirname(__FILE__) . "../..//biblioteca/firmas"; // carpeta donde se almacena el archivo
+$url_target = str_replace('\\', './', $ruta_insertada) . '/' .$firma;
+
+// crear la carpta si no existe
+
+if (!file_exists($ruta_insertada)) {
+    mkdir($ruta_insertada, 0777, true);
+};
+
+
+if (move_uploaded_file($url_temporal, $url_target)) {
+    echo "El archivo " . htmlspecialchars(basename($firma)) . " ha sido cargado con éxito.";
+} else {
+    echo "Ha habido un error al cargar tu archivo.";
 }
-if($apellido==""){
-    echo "!OOPSSS OCURRIO UN ERROR!,El apellido es obligatorio completar,error";
-    exit();
+
+$tipo_archivo = strtolower(pathinfo($firma,PATHINFO_EXTENSION)); //Extensión de nuestro archivo 
+
+if($tipo_archivo!= "jpg" && $tipo_archivo!= "jpeg" && $tipo_archivo!= "png" ) {
+    echo "Solo se permiten imágenes tipo JPG, JPEG, PNG ";
 }
-if($celular==""){
-    echo "!OOPSSS OCURRIO UN ERROR!,El celular es obligatorio completar,error";
-    exit();
-}
-if($instruccion==""){
-    echo "!OOPSSS OCURRIO UN ERROR!,El instrucctor es obligatorio completar,error";
-    exit();
-}
-if(empty($firma)){
-    echo "!OOPSSS OCURRIO UN ERROR!,El firmar es obligatorio completar,error";
-    exit();
-}
+
 
 //esto me sirve para registrar un dato en la bd
 
